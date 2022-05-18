ed9113452fed21d7e49b85b68f6be194a8f4b881
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-18 17:41:50 -0400

    logic for signup button enabled/disabled

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,19 +1,38 @@
 import { Component } from "react";
 
 class SignUpPage extends Component {
+  state = {
+    disabled: true,
+  };
+
+  onChangePassword = (event) => {
+      const currentValue = event.target.value
+      this.setState({
+          password: currentValue,
+          disabled: currentValue !== this.state.passwordRepeat
+      })
+  }
+
+  onChangePasswordRepeat = (event) => {
+      const currentValue = event.target.value
+      this.setState({
+          passwordRepeat: currentValue,
+          disabled: currentValue !== this.state.password
+      })
+  }
+
   render() {
     return (
       <div>
-        <h1>Sign Up</h1>;
-        <label htmlFor="username">Username</label>
-        <input id="username"/>
+        <h1>Sign Up</h1>;<label htmlFor="username">Username</label>
+        <input id="username" />
         <label htmlFor="email">E-mail</label>
-        <input id="email"/>
+        <input id="email" />
         <label htmlFor="password">Password</label>
-        <input id="password" type='password' />
+        <input id="password" type="password" onChange={this.onChangePassword} />
         <label htmlFor="passwordRepeat">Password Repeat</label>
-        <input id="passwordRepeat" type='password' />
-        <button disabled>Sign Up</button>
+        <input id="passwordRepeat" type="password" onChange={this.onChangePasswordRepeat} />
+        <button disabled={this.state.disabled}>Sign Up</button>
       </div>
     );
   }
