2e433fb5a35e4b9960fbe74abfd02d2e490a9097
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-18 19:36:37 -0400

    adding submit method to send user inputs to backend via axios

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,42 +1,80 @@
 import { Component } from "react";
+import axios from "axios";
 
 class SignUpPage extends Component {
   state = {
-      password: '', // we can have this set to empty just for reference
-      passwordRepeat: '', // we can have this set to empty just for reference
+    username: "",
+    email: "",
+    password: "", // we can have this set to empty just for reference
+    passwordRepeat: "", // we can have this set to empty just for reference
   };
 
+  onChangeUsername = (event) => {
+    const currentValue = event.target.value;
+    this.setState({
+      username: currentValue,
+    });
+  };
+  onChangeEmail = (event) => {
+    const currentValue = event.target.value;
+    this.setState({
+      email: currentValue,
+    });
+  };
   onChangePassword = (event) => {
-      const currentValue = event.target.value
-      this.setState({
-          password: currentValue,
-      })
-  }
+    const currentValue = event.target.value;
+    this.setState({
+      password: currentValue,
+    });
+  };
 
   onChangePasswordRepeat = (event) => {
-      const currentValue = event.target.value
-      this.setState({
-          passwordRepeat: currentValue,
-      })
-  }
+    const currentValue = event.target.value;
+    this.setState({
+      passwordRepeat: currentValue,
+    });
+  };
+
+  submit = (event) => {
+    event.preventDefault()
+    const { username, email, password } = this.state;
+    const body = {
+      username,
+      email,
+      password,
+    };
+    axios.post("/api/1.0/users", body);
+  };
 
   render() {
-      let disabled = true;
-      const { password, passwordRepeat } = this.state;
-      if (password && passwordRepeat) {
-          disabled = password !== passwordRepeat;
-      }
-      return (
+    let disabled = true;
+    const { password, passwordRepeat } = this.state;
+    if (password && passwordRepeat) {
+      disabled = password !== passwordRepeat;
+    }
+    return (
       <div>
-        <h1>Sign Up</h1>;<label htmlFor="username">Username</label>
-        <input id="username" />
-        <label htmlFor="email">E-mail</label>
-        <input id="email" />
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" onChange={this.onChangePassword} />
-        <label htmlFor="passwordRepeat">Password Repeat</label>
-        <input id="passwordRepeat" type="password" onChange={this.onChangePasswordRepeat} />
-        <button disabled={disabled}>Sign Up</button>
+        <form>
+          <h1>Sign Up</h1>;<label htmlFor="username">Username</label>
+          <input id="username" onChange={this.onChangeUsername} />
+          <label htmlFor="email">E-mail</label>
+          <input id="email" onChange={this.onChangeEmail} />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={this.onChangePassword}
+          />
+          <label htmlFor="passwordRepeat">Password Repeat</label>
+          <input
+            id="passwordRepeat"
+            type="password"
+            onChange={this.onChangePasswordRepeat}
+          />
+          <button onClick={this.submit} disabled={disabled}>
+            Sign Up
+          </button>
+        </form>
       </div>
     );
   }
