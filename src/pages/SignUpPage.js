ed2be84fba50a9e15c0e411e29dea051e0d0aafc
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-23 22:05:03 -0400

    prettier, adding Bootstrap classes to our page for styling

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -10,14 +10,15 @@ class SignUpPage extends Component {
   };
 
   onChange = (event) => {
-      const { id, value } = event.target; // destructuring the event.target into id and value
-      this.setState({ // setting the state values of the "id's"
-          [id]: value // to the value from the onChange event (aka. user's input)
-      })
-  }
+    const { id, value } = event.target; // destructuring the event.target into id and value
+    this.setState({
+      // setting the state values of the "id's"
+      [id]: value, // to the value from the onChange event (aka. user's input)
+    });
+  };
 
   submit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const { username, email, password } = this.state;
     const body = {
       username,
@@ -34,27 +35,58 @@ class SignUpPage extends Component {
       disabled = password !== passwordRepeat;
     }
     return (
-      <div>
-        <form>
-          <h1>Sign Up</h1>;<label htmlFor="username">Username</label>
-          <input id="username" onChange={this.onChange} />
-          <label htmlFor="email">E-mail</label>
-          <input id="email" onChange={this.onChange} />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            onChange={this.onChange}
-          />
-          <label htmlFor="passwordRepeat">Password Repeat</label>
-          <input
-            id="passwordRepeat"
-            type="password"
-            onChange={this.onChange}
-          />
-          <button onClick={this.submit} disabled={disabled}>
-            Sign Up
-          </button>
+      <div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+        <form className="card mt-5">
+          <div className="card-header">
+            <h1 className="text-center">Sign Up</h1>
+          </div>
+          <div className="card-body">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                id="username"
+                className="form-control"
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email">E-mail</label>
+              <input
+                className="form-control"
+                id="email"
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password">Password</label>
+              <input
+                className="form-control"
+                id="password"
+                type="password"
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="passwordRepeat">Password Repeat</label>
+              <input
+                className="form-control"
+                id="passwordRepeat"
+                type="password"
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="text-center">
+              <button
+                className="btn btn-primary"
+                onClick={this.submit}
+                disabled={disabled}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </form>
       </div>
     );
