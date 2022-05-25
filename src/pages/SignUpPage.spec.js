6da813a50e1d6028802de649aa51797b1217820a
Peter Lewandowski <mapiotr2010@gmail.com>
2022-05-25 16:05:10 -0400

    added test for displaying account activation notification, fixed console warning (library unmounts component after the Promise resolves after 500ms)

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -87,7 +87,7 @@ describe("Sign Up Page", () => {
       setup();
       userEvent.click(button);
 
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await screen.findByText("Please check your email to activate your account")
 
       expect(requestBody).toEqual({
         //make sure that the request body and response body is either: both stringified or parsed
@@ -126,6 +126,22 @@ describe("Sign Up Page", () => {
       userEvent.click(button);
       const spinner = screen.getByRole("status");
       expect(spinner).toBeInTheDocument();
+      await screen.findByText("Please check your email to activate your account")
+    });
+
+    it("displays account activation notification after successful sign up request", async () => {
+      const server = setupServer(
+        rest.post("/api/1.0/users", (req, res, ctx) => {
+          return res(ctx.status(200));
+        })
+      );
+      server.listen();
+      setup();
+      const message = "Please check your email to activate your account";
+      expect(screen.queryByText(message)).not.toBeInTheDocument();
+      userEvent.click(button);
+      const text = await screen.findByText(message);
+      expect(text).toBeInTheDocument();
     });
   });
 });
